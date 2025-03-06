82947844967b89234d66a4dbab075589b5b15d60
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-03-06 06:51:38 +0400

    additional questions

--- template/.devcontainer/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#apt update
-#apt install software-properties-common apt-transport-https ca-certificates curl
-
-#add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
-#curl -fsSL https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc
-#gpg --dearmor -o /usr/share/keyrings/r-archive-keyring.gpg
